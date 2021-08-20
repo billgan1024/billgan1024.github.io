f2ec62fd25df41f81139c3ba0c36ef7f683e5e7a
Bill Gan <billgan12345@gmail.com>
2021-08-20 11:43:06 -0400

    update footer

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Card({ children, title, src, url }) {
+    //since there are 4 cards on screen, whenever each one is mounted, the code will run
+    //think of it as the object's create event
+    useEffect(() => {
+        console.log("hi");
+    }, []);
     return (
         <div>
             <h3 className="fade-in" style={{ marginBottom: "-1vh" }}>{title}</h3>
